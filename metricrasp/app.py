36d5633a39229085f234f9fd978e43cf5c7de3f1
asdf <migsking@naver.com>
2020-06-21 03:15:09 +0900

    [ADD] url

--- metricrasp/app.py
+++ metricrasp/app.py
@@ -2,7 +2,12 @@ import metricrasp.extensions
 
 from flask import Flask
 
+from metricrasp.apis import auth
+from metricrasp.apis import metric
+
 def register_api(app):
+    app.register_blueprint(auth.api.blueprint)
+    app.register_blueprint(metric.api.blueprint)
     return None
 
 def register_extension(app):
@@ -15,7 +20,7 @@ def set_config(app, host_config):
     return None
 
 def create_app(host_config):
-    app = Flask("metricrasp", __name__)
+    app = Flask(__name__)
     set_config(app, host_config)
     register_extension(app)
     register_api(app)
