c42c3d8299205fdc1e7333375d5f181cf868e27b
asdf <migsking@naver.com>
2020-06-21 02:10:56 +0900

    [ADD] Login Logic

--- metricrasp/apis/auth.py
+++ metricrasp/apis/auth.py
@@ -1,5 +1,8 @@
 from flask import Blueprint, request
 from flask_restful import Api, Resource
+from flask_jwt_extended import create_access_token
+
+from metricrasp.config import HostConfig
 
 
 api = Api(Blueprint(__name__, __name__))
@@ -8,3 +11,5 @@ api = Api(Blueprint(__name__, __name__))
 class Auth(Resource):
     def post(self):
         password = request.json("password")
+        if not password == HostConfig.PASSWORD: abort(409)
+        return create_access_token(identity="admin")
