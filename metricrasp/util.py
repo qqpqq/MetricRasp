4009cd7a347486a47026abf3e0ebc6f4ad9074e2
asdf <migsking@naver.com>
2020-06-21 03:04:48 +0900

    [MODIFY] Exception

--- metricrasp/util.py
+++ metricrasp/util.py
@@ -5,7 +5,7 @@ def json_parser(json_file_path):
     try:
         with open(json_file_path) as json_file:
             json_data = json.load(json_file)
-    except:
-        raise "The conf file does not exist"
+    except Exception:
+        raise Exception
 
     return json_data
